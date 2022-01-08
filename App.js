1b3f6c43e8f4044eeffdf51f190161053943fe3a
muzammil <maliikkath78@gmail.com>
2022-01-09 03:27:52 +0500

    Notes-App

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Home from './screens/Home';
+import Detail from './screens/DetailNote';
+import AddNote from './screens/AddNote';
+import { initializeDatabase } from './db/db';
 
 export default function App() {
+  const [loaded] = useFonts({
+    "Poppins": require("./assets/fonts/regular.ttf"),
+    "Semibold": require("./assets/fonts/semibold.ttf")
+  });
+
+  if (!loaded) {
+    return null;
+  }
+  initializeDatabase()
+    .then(() => {
+      console.log('Initialized database');
+    })
+    .catch(err => {
+      console.log('Initializing db failed.');
+      console.log(err);
+    });
+  const Stack = createNativeStackNavigator()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="DetailNote" component={Detail} />
+        <Stack.Screen name="AddNote" component={AddNote} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
